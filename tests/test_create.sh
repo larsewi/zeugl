37f4ff9f53360d97a6452a8de548463fa2d51977
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-08-04 15:21:26 +0200

    test_create.sh: Clean up test
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- tests/test_create.sh
+++ tests/test_create.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-src_dir="$(dirname "$0")/.."
-echo "Hello zeugl" | $src_dir/cli/zeugl -d -c 644 testfile.txt
-stat testfile.txt
+EXP_MODE=644
+echo "Hello zeugl" | zeugl -d -c $EXP_MODE testfile.txt
 
-env
-
-mode=$(stat testfile.txt --format %a)
-if [ "$mode" != 644 ]; then
-    echo "Error: Expected mode 644, but got $mode" 1>&2
+ACT_MODE=$(stat testfile.txt --format %a)
+if [ "$ACT_MODE" != "$EXP_MODE" ]; then
+    echo "$(basename "$0"): error: Expected mode $EXP_MODE, but got $ACT_MODE" 1>&2
     exit 1
 fi
