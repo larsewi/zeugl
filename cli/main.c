b3b75237a02715c7f3d1524870291589f83598c8
Lars Erik Wik <lars.erik.wik@northern.tech>
2025-09-05 17:49:27 +0200

    WIP: working on usage string and help string
    Signed-off-by: Lars Erik Wik <lars.erik.wik@northern.tech>

--- cli/main.c
+++ cli/main.c
@@ -1,5 +1,6 @@
 #include "config.h"
 
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdbool.h>
@@ -12,19 +13,64 @@
 #include "logger.h"
 #include "zeugl.h"
 
-#define PRINT_USAGE(prog)                                                      \
-  fprintf(stderr,                                                              \
-          "Usage: %s [-f INPUT_FILE] [-c MODE] [-a] [-t] [-d] [-v] [-h] "      \
-          "OUTPUT_FILE\n",                                                     \
-          prog)
+#define USAGE_STRING                                                           \
+  "Usage: %s [-f INPUT_FILE] [-c MODE] [-a] [-t] [-b] [-d] [-v] [-h] "         \
+  "OUTPUT_FILE\n"
+
+typedef struct {
+  const char *opt;
+  const char *arg;
+  const char *desc;
+} option;
+
+static const option options[] = {
+    {"f", "FILENAME", "input file ('-' for stdin)"},
+    {"c", "MODE", "create file if it does not exist"},
+    {"a", NULL, "position file offset at end-of-file"},
+    {"t", NULL, "truncate file"},
+    {"b", NULL, "don't block on advisory locks or if interrupted"},
+    {"d", NULL, "enable debug logging (noop if compiled with NDEBUG=1)"},
+    {"v", NULL, "print version name and exit"},
+    {"h", NULL, "print this help menu"},
+};
 
 int main(int argc, char *argv[]) {
   const char *input_fname = "-";
   int flags = 0;
   mode_t mode = 0;
 
+  char opt_str[BUFSIZ];
+  int opt_str_cursor = 0;
+  char usage_str[BUFSIZ];
+  int usage_str_cursor;
+
+  int ret = snprintf(usage_str, sizeof(usage_str), "Usage: %s", argv[0]);
+  assert((ret >= 0) && (ret < sizeof(usage_str)));
+  usage_str_cursor += ret;
+
+  for (int i = 0; i < sizeof(options) / sizeof(option); i++) {
+    /* Create option string for getopt() */
+    if (options[i].arg == NULL) {
+        ret = snprintf(opt_str + opt_str_cursor, sizeof(opt_str) - opt_str_cursor, " [-%s]", options[i].opt);
+    }
+    else {
+        ret = snprintf(opt_str + opt_str_cursor, sizeof(opt_str) - opt_str_cursor, " [-%s %s]", options[i].opt, options[i].arg);
+    }
+    assert((ret >= 0) && (ret < sizeof(opt_str)));
+    opt_str_cursor += ret;
+
+    opt_str[opt_str_cursor++] = *options[i].opt;
+    if (options[i].arg != NULL) {
+      opt_str[opt_str_cursor++] = ':';
+    }
+    opt_str[opt_str_cursor] = '\0';
+
+    /* Create usage string */
+    ret = snprintf(usage_str + usage_str_cursor, sizeof(usage_str) - usage_str_cursor, "\t-%s  %s\n", options[i].opt, options[i].desc);
+  }
+
   int opt;
-  while ((opt = getopt(argc, argv, "f:c:atdvh")) != -1) {
+  while ((opt = getopt(argc, argv, opt_str)) != -1) {
     switch (opt) {
     case 'f':
       input_fname = optarg;
@@ -51,6 +97,9 @@ int main(int argc, char *argv[]) {
     case 't':
       flags |= Z_TRUNCATE;
       break;
+    case 'b':
+      flags |= Z_NOBLOCK;
+      break;
     case 'd':
 #if !NDEBUG
       LoggerEnable();
@@ -60,17 +109,17 @@ int main(int argc, char *argv[]) {
       printf("%s\n", PACKAGE_STRING);
       return EXIT_SUCCESS;
     case 'h':
-      PRINT_USAGE(argv[0]);
+      printf(USAGE_STRING "\n", argv[0]);
       return EXIT_SUCCESS;
     default:
-      PRINT_USAGE(argv[0]);
+      fprintf(stderr, USAGE_STRING, argv[0]);
       return EXIT_FAILURE;
     }
   }
 
   if (optind >= argc) {
     fprintf(stderr, "Missing output file argument\n");
-    PRINT_USAGE(argv[0]);
+    fprintf(stderr, USAGE_STRING, argv[0]);
     return EXIT_FAILURE;
   }
   const char *output_fname = argv[optind++];
